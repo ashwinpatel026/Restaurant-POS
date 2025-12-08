77e06a55e6332743dece3e86c543c9e30f2a8b9e
aarpus <prj.dev@aarpus.net>
2025-12-08 06:59:34 -0500

    user access update

--- scripts/seed-simple.mjs
+++ scripts/seed-simple.mjs
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
 
-// Load environment variable
+// Load environment variables
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 dotenv.config({ path: join(__dirname, '..', '.env') })
